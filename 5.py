e06d6a02fb09e588a5494d812fd22cd63463a855
sejal <sejalvishnoi32@gmail.com>
2023-04-07 22:58:59 +0530

    this file commit message

--- 5.py
+++ 5.py
@@ -4,3 +4,6 @@ d2 = {"sejal":"maggi","prince":"Roti","jay":"pasta","shubham":{"ankit":"sendwich
 d2 ["neha"]=["junk food"]
 del d2["neha"]
 print(d2)
+d2.update({"leena":"toffi"})
+print(d2) 
+print(d2.keys())
